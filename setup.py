b8eb8b0f6e5d1c3faf16e5ba1976aef4df1047ae
S. Joshua Swamidass <swamidass@gmail.com>
2022-08-29 16:03:54 -0500

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(name='PlotDot',
+      version='0.0',
+      description='Utility for plotting perceputally uniform dots.',
+      author='swamidass',
+      author_email='swamidass@wustl.edu',
+      url='https://github.com/swamidasslab/plotdot/',
+      packages=['plotdot'],
+     )
