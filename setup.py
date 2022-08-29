5ec570df6ef3a538fbda0d764ad0d915659b3928
S. Joshua Swamidass <swamidass@gmail.com>
2022-08-29 16:29:29 -0500

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='PlotDot',
-      version='0.0',
+      version='0.0.1',
       description='Utility for plotting perceputally uniform dots.',
       author='swamidass',
       author_email='swamidass@wustl.edu',
