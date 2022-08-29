e8fbb07ad9de5b373b15a4c153cf93f20a3a7d8d
S. Joshua Swamidass <swamidass@gmail.com>
2022-08-29 18:39:05 -0500

    increment version number

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 
 from setuptools import setup
 
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(name='PlotDot',
-      version='0.0.3',
-      description='Utility for plotting perceputally uniform dots.',
-      author='swamidass',
-      author_email='swamidass@wustl.edu',
-      url='https://github.com/swamidasslab/plotdot/',
-      packages=['plotdot'],
+    version='0.0.4',
+    description='Utility for plotting perceputally uniform dots.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',     
+	author='S. Joshua Swamidass',
+    author_email='swamidass@wustl.edu',
+    url='https://github.com/swamidasslab/plotdot/',
+    packages=['plotdot'],
      )
