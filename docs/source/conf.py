9680991f6dab57384f3f52b1982b88f61abfe0b9
S. Joshua Swamidass <swamidass@gmail.com>
2022-09-21 15:39:25 -0500

    doc add nb andjuptext

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,32 +10,47 @@
 import sys
 import os
 import xenopict
-sys.path.insert(0, os.path.abspath('../..'))
 
 
-project = 'XenoPict'
-copyright = '2022, S. Joshua Swamidass'
-author = 'S. Joshua Swamidass'
+source_suffix = [".rst", ".md"]
+
+sys.path.insert(0, os.path.abspath("../.."))
+
+
+project = "XenoPict"
+copyright = "2022, S. Joshua Swamidass"
+author = "S. Joshua Swamidass"
 release = xenopict.__version__
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx_rtd_theme',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx_rtd_theme",
+    "nbsphinx",
 ]
+import jupytext
 
-templates_path = ['_templates']
-exclude_patterns = []
+nbsphinx_custom_formats = {
+    ".md": lambda s: jupytext.reads(s, ".md"),
+}
+
+
+templates_path = ["_templates"]
+exclude_patterns = ["build"]
+
+nbsphinx_execute = "always"
 
 autosummary_generate = True
 
+
+html_sourcelink_suffix = ""
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ['_static']
+html_static_path = ["_static"]
